a20c04590c7c8278353d4010eb832014ae0b3fc9
OriolAbril <oriol.abril.pla@gmail.com>
2016-11-26 02:21:29 +0100

    Friday 25/11/16 work, mainly Prac4

--- Prac4/initiate_mc.f
+++ Prac4/initiate_mc.f
@@ -4,14 +4,14 @@
 !     1. Defining variables and dimensions
       implicit none
       integer, parameter :: unit=1
-      integer :: ncycle,natoms
+      integer :: ncycle,natoms,nsample
       real(8) :: temp,sigma,epsil,delta,ro
 
 
 !     2. Reading data and computing related quantities
 
       open(1,file='lj-monte.data',status='old')
-      read(1,*) ncycle
+      read(1,*) ncycle,nsample
       read(1,*) natoms
       read(1,*) temp
       read(1,*) sigma,epsil
